2e4c68efb3777d9a3efcafa4b0263f1b79920c77
Ryan Shirley <RJSDEVMAGIC@GMAIL.COM>
2018-08-11 21:17:55 -0400

    Complete.4now
    Got functionality with Java script/Html... phone Number and Age tag id.  
    The ID is complete.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function idCard() {
+function idCard(){
   var first = document.getElementById('firstName').value;
   var last = document.getElementById('lastName').value;
-  var addy = document.getElementById('Address').value;
-  var idCard = "Hi" + first +""+last+""+addy;
-  document.getElementById('postFullName').innerHTML = idCard;
+  var Address = document.getElementById('Address').value;
+  var idCard = "I am " + first +" "+ last;
+  var idCard1 = Address;
+
+  document.getElementById("postFullName").innerHTML = idCard ;
+  document.getElementById("postAddress").innerHTML = idCard1 ;
+
+  var Age = document.getElementById('Age').value;
+  var phoneNumber = document.getElementById('phoneNumber').value;
+  var numberArray = [Age, phoneNumber];
+
+  for (var i=0; i < numberArray.length; i++){
+    if (numberArray[i] <= 100){
+      document.getElementById('postAge').innerHTML = Age;
+    }
+
+      if (numberArray[i] > 100){
+        document.getElementById('postPhoneNumber').innerHTML = phoneNumber;
+    }
+  }
 }
